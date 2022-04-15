98e91b1b23265812e4a4181324f5551f9fd4f1fa
thomasfrs <thomasfrs44@gmail.com>
2022-04-15 10:27:00 +0200

    Modified main.py for deployement purpose

--- main.py
+++ main.py
@@ -43,20 +43,21 @@ async def on_ready():
 async def on_message(message):
     if message.author != client.user:
         # prints out every message
-        print(
-            "%s (%s, %s, %s, %s)" %
-            (datetime.now().strftime("%H:%M:%S"),
-             message.guild.name,
-             message.channel.name,
-             message.author.name,
-             message.content))
+        # print(
+        # "%s (%s, %s, %s, %s)" %
+        # (datetime.now().strftime("%H:%M:%S"),
+        # message.guild.name,
+        # message.channel.name,
+        # message.author.name,
+        # message.content))
 
         # useful commands
         # detects if there any uwu or owo in the message
         if any(elt in message.content for elt in tt.list_permutations("uwu")) or any(
                 elt in message.content for elt in tt.list_permutations("owo")):
             translation = tt.text_translation(message.content)
-            print("\tIdentified uwu/owo in content")
+            print("%s: Identified uwu/owo in content" %
+                  (datetime.now().strftime("%H:%M:%S")))
             print("\tSent translation: %s" % (translation))
             await message.channel.send(translation)
 
@@ -64,7 +65,8 @@ async def on_message(message):
         elif message.content.startswith(bot_prefix + "say"):
             translation = tt.text_translation(
                 message.content[len(bot_prefix) + 4:])
-            print("\tIdentified %ssay command" % bot_prefix)
+            print("%s: Identified %ssay command" %
+                  (datetime.now().strftime("%H:%M:%S"), bot_prefix))
             print("\tDeleted message from %s" % message.author.name)
             await message.delete()
             print("\tSent translation: %s" % (translation))
@@ -72,29 +74,33 @@ async def on_message(message):
 
         elif message.content.startswith(bot_prefix + "gif"):
             gif_url = mr.retrieve_gif("owo", 20, api_key)
-            print("\tIdentified %sgif command" % bot_prefix)
+            print("%s: Identified %sgif command" %
+                  (datetime.now().strftime("%H:%M:%S"), bot_prefix))
             print("\tSent gif: %s" % (gif_url))
             await message.channel.send(gif_url)
 
         elif message.content.startswith(bot_prefix + "cpasta"):
             copypasta_translation = tt.text_translation(
                 mr.retrieve_copypasta("copypastas.txt"))
-            print("\tIdentified %scpasta command" % bot_prefix)
+            print("%s: Identified %scpasta commands" %
+                  (datetime.now().strftime("%H:%M:%S"), bot_prefix))
             print("\tSent copypasta: %s" % (copypasta_translation))
             await message.channel.send(copypasta_translation)
 
         # hidden commands
         elif message.content.startswith(bot_prefix + "stats"):
             uptime = datetime.now() - start_time
-            print("\tIdentified %sstats command" % bot_prefix)
+            print("%s: Identified %sstats command" %
+                  (datetime.now().strftime("%H:%M:%S"), bot_prefix))
             print("\tDeleted message from %s" % message.author.name)
             await message.delete()
-            print("Uptime: %s\nServers: %s" %
+            print("\tUptime: %s\n\tServers: %s" %
                   (uptime, str(len(client.guilds))))
             await message.channel.send("Uptime: %s\nServers: %s" % (uptime, str(len(client.guilds))))
 
         elif message.content.startswith(bot_prefix + "read"):
-            print("\tIdentified %sread command" % bot_prefix)
+            print("%s: Identified %sread command" %
+                  (datetime.now().strftime("%H:%M:%S"), bot_prefix))
             print("\tDeleted message from %s" % message.author.name)
             await message.delete()
             try:
@@ -108,16 +114,20 @@ async def on_message(message):
 
         # useless commands
         elif message.content.startswith(bot_prefix + "whoru"):
-            print("\tIdentified %swhoru command" % bot_prefix)
-            await message.channel.send(tt.text_translation("Hello! My name is ") + "{0.user}".format(client))
-            await message.channel.send(tt.text_translation("My pronouns are (NVIDIA GeForce RTX 2060 Intel Core i5-10400F CPU 16.0GB RAM/ them)."))
-            await message.channel.send(tt.text_translation("My main purpose is to translate your text into its something very UwU."))
-            await message.channel.send(tt.text_translation("I have been created by Anne Frs in only one day! Which probably explains why my organism\nis a complete mess and I can't do much :("))
+            print("%s: Identified %swhoru command" %
+                  (datetime.now().strftime("%H:%M:%S"), bot_prefix))
+            await message.channel.send(tt.text_translation("Hello! My name is ") + "{0.user}\nMy pronouns are (NVIDIA GeForce RTX 2060 Intel Core i5-10400F CPU 16.0GB RAM/ them).\nMy main purpose is to translate your text into its something very UwU.\nI have been created by Anne Frs in only one day! Which probably explains why my organism\nis a complete mess and I can't do much :(".format(client))
 
         elif message.content.startswith(bot_prefix + "commands"):
-            print("\tIdentified %scommands command" % bot_prefix)
+            print("%s: Identified %scommands command" %
+                  (datetime.now().strftime("%H:%M:%S"), bot_prefix))
             await message.channel.send("*Prefix:* ``%s``\n*Commands:*\n``%swhoru``: I talk extensively about myself because I don't have a gf\n``%scommands``: I display this message once again, because why not\n``your_message containing uwu/owo``: I reply to your message and translate it into its UwU and OwO form\n``%ssay + your_message``: I delete your message and translate it into its UwU and OwO form\n``%sgif``: I post a random gif that is very much OwO\n``%scpasta``: I post a random copypasta that is very much OwO" % (bot_prefix, bot_prefix, bot_prefix, bot_prefix, bot_prefix, bot_prefix))  # bot_prefix
 
+try:  # if you're working on repl.it
+    os.getenv()
+    discord_token = os.getenv("discord_token")
+    api_key = os.getenv("api_key")
+except BaseException:  # if you're working on your usual ide
+    discord_token, api_key = get_tokens()
 
-discord_token, api_key = get_tokens()
 client.run(discord_token)
